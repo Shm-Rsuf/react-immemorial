05dda412e0758ceef30d206418927713fc5d74de
shm_usuf <shmusuf@gmail.com>
2022-12-07 13:21:46 +0600

    all setup done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
