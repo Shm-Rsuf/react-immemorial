f664c7d04c5f9d7db6b6f368fd6b403bc2ee4901
shm_usuf <shmusuf@gmail.com>
2022-12-11 00:25:32 +0600

    feature added

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { useRef } from "react";
+import {
+  useGsapFeatureLeftShutterUnvail,
+  useGsapFeatureRightShutterUnvail,
+} from "../hooks/gsap";
 
 const Featured = () => {
-  return <div>Featured</div>;
+  const featureRef = useRef(null);
+  const featureLeftShutter = useRef(null);
+  const featureRightShutter = useRef(null);
+
+  useGsapFeatureLeftShutterUnvail(featureLeftShutter, featureRef);
+  useGsapFeatureRightShutterUnvail(featureRightShutter, featureRef);
+
+  return (
+    <section className="featured wrapper" ref={featureRef}>
+      <h2 className="section-title">featured</h2>
+      <div className="features">
+        <div className="feature-left">
+          <span className="feature-text">90'S TELEPHONE</span>
+          <img
+            src="https://images.pexels.com/photos/2587464/pexels-photo-2587464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="90'S TELEPHONE"
+          />
+          <span
+            className="feature-shutter-left"
+            ref={featureLeftShutter}
+          ></span>
+        </div>
+        <div className="feature-right">
+          <span className="feature-text">90'S CASSETTE PLAYER</span>
+          <img
+            src="https://images.pexels.com/photos/6076410/pexels-photo-6076410.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="90'S CASSETTE PLAYER"
+          />
+          <span
+            className="feature-shutter-right"
+            ref={featureRightShutter}
+          ></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Featured;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,63 +6,6 @@ const Home = () => {
     <div>
       <Hero />
       <Featured />
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi sint
-        ad harum amet dignissimos deserunt soluta vero quia, iste aspernatur
-        placeat possimus architecto aliquam officia quos. Temporibus pariatur
-        officia aut officiis quod deleniti, tempora iusto. Veritatis voluptas
-        atque consectetur minus omnis, itaque tenetur! Doloremque, recusandae
-        nobis. Soluta cumque, corrupti suscipit iusto a deleniti officia illum
-        distinctio minus eum, iure consequuntur in adipisci magni? Voluptas
-        dicta id magnam harum excepturi laboriosam quas sit, blanditiis dolorum,
-        maiores corporis itaque! Veritatis animi qui est sequi id ullam illo
-        aspernatur sapiente suscipit excepturi architecto nostrum, nam minima
-        quod. Maxime culpa beatae voluptatum, non obcaecati libero totam dolores
-        amet, saepe accusamus eum itaque? Quibusdam, unde. Odit, obcaecati dolor
-        corporis voluptatem est, quibusdam tempore nihil, praesentium expedita
-        eaque labore? Id facere voluptatem nemo error ut vel atque officia
-        mollitia laudantium adipisci ullam beatae, laboriosam nihil! Doloremque
-        distinctio consequuntur, ad dignissimos quis, error id hic possimus
-        consectetur commodi pariatur quasi aliquid laborum dolores facilis rem
-        inventore. Ex, animi. Distinctio aut cum sit ea fugiat maxime voluptas
-        dignissimos ratione nostrum eos laboriosam, dolores dolore accusamus
-        error alias laborum ad. Sed a asperiores eveniet non aut cumque totam
-        molestias possimus. Velit fugit eius obcaecati fugiat ea pariatur, sed
-        explicabo magnam ipsum, cumque accusantium? Voluptas unde sunt,
-        doloribus odit at repellendus dignissimos officiis fugit a qui eligendi
-        soluta omnis deserunt dolores voluptatem similique. Dolorem,
-        consequatur. Minima totam ab ex aliquam! Quaerat, ea tenetur? Dolor,
-        eligendi! Similique aliquid earum optio! Distinctio earum dolorum fuga
-        voluptatum facilis autem aperiam odit ut ullam, officiis dignissimos,
-        assumenda corporis nobis architecto quasi at recusandae quidem placeat
-        officia quis dicta odio? In, odit aspernatur tenetur consequatur
-        molestias ipsa corrupti incidunt sequi, voluptates deserunt eos soluta
-        sunt dolor! Saepe nisi velit debitis iste omnis nam deserunt quod
-        repellendus id praesentium libero, eaque hic, ducimus asperiores eum
-        natus consequatur tempore laborum. Sapiente, in expedita tenetur enim
-        illo dolorem, officiis quam corporis, illum sed soluta nisi. Porro
-        aliquam sapiente saepe iure eligendi quas non. Corporis earum explicabo
-        quia doloremque nemo corrupti quas voluptatum animi eius repellendus.
-        Cumque iusto ad asperiores saepe delectus dolorem adipisci veritatis
-        exercitationem ducimus quibusdam natus voluptate amet corporis,
-        consectetur velit enim? Eligendi dolor impedit quidem corrupti et ab
-        maxime recusandae eveniet aut tempore, qui maiores? Porro, quos quis
-        mollitia consectetur quod quidem dolorem iure beatae, rem nihil aut
-        corrupti eaque repellat harum repudiandae aliquam veniam omnis,
-        inventore suscipit totam? A quis corrupti numquam aut debitis
-        repellendus, hic, odit esse maiores voluptates soluta magni deserunt
-        iste veniam veritatis culpa et! Hic, vel, fugiat sit unde cupiditate
-        molestias, ipsam velit exercitationem temporibus adipisci alias
-        similique quos placeat officia. Ratione praesentium eius ea nulla? Ipsam
-        nostrum consectetur nam inventore asperiores ipsa, pariatur minima
-        excepturi alias, voluptatum blanditiis veniam quas fugiat quibusdam
-        tempora consequuntur dolorum corporis quae quia vero ducimus culpa
-        facere a. Assumenda magnam voluptatem necessitatibus odio, debitis quis
-        amet recusandae deserunt iusto saepe libero ullam non sequi cupiditate
-        enim reprehenderit ducimus earum molestiae dolores eligendi voluptate
-        nulla. Totam vitae perferendis, mollitia laborum beatae inventore
-        numquam quos nisi.
-      </p>
     </div>
   );
 };
